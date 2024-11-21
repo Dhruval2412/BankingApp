f3dee6611273701b38e2741386ac2f9b77ff1421
Dhruval Makwana <makwanadhruval1@gmail.com>
2024-11-21 08:05:26 +0530

    Update SecurityConfig.java

--- project/bank/Bank-App-start/Bank-App-start/src/main/java/com/example/bankapp/config/SecurityConfig.java
+++ project/bank/Bank-App-start/Bank-App-start/src/main/java/com/example/bankapp/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/register").permitAll()
+                        .requestMatchers("/register","/login").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
