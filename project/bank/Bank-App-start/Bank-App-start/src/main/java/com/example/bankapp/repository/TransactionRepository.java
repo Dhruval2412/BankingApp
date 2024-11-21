ff5efb82df46576622556618df6d0f5aabd4edb6
Dhruval Makwana <makwanadhruval1@gmail.com>
2024-11-21 07:58:28 +0530

    Update TransactionRepository.java
    update the new features search transaction

--- project/bank/Bank-App-start/Bank-App-start/src/main/java/com/example/bankapp/repository/TransactionRepository.java
+++ project/bank/Bank-App-start/Bank-App-start/src/main/java/com/example/bankapp/repository/TransactionRepository.java
@@ -3,9 +3,12 @@ package com.example.bankapp.repository;
 import com.example.bankapp.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
     List<Transaction> findByAccountId(Long accountId);
+    List<Transaction> findByAccountIdAndType(Long accountId, String type);
+    List<Transaction> findByAccountIdAndTimestampBetween(Long accountId, LocalDate startDate, LocalDate endDate);
 
 }
